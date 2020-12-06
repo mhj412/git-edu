6941ac87126b45f11c34057d5ad463275edba52b
mhj412 <heongjoo14@gmail.com>
2020-12-06 16:45:28 +0900

    title&footer&menu group

--- src/App.vue
+++ src/App.vue
@@ -8,49 +8,37 @@
       
     
     <v-app-bar-nav-icon @click="drawer=!drawer"/>
-    <v-toolbar-title>page title</v-toolbar-title>
+    <site-title :title="title"></site-title>
     <v-spacer/>
-    <v-btn icon to="/about">
-      <v-icon>mdi-magnify</v-icon>
-    </v-btn>
-     <v-btn icon to="/">
-      <v-icon>mdi-magnify</v-icon>
-    </v-btn>
     </v-app-bar>
     <v-navigation-drawer app v-model="drawer">
-         <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="title">
-            Application
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            subtext
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-
+        
+      <v-divider></v-divider>
+      <site-menu></site-menu>
       
     </v-navigation-drawer>
     <v-content>
         <router-view/>
     </v-content>
-    <v-footer app color="primary" dark absolute>
-      <v-spacer></v-spacer>
-      <div>&copy; {{ new Date().getFullYear() }}</div>
-    </v-footer>
+   <site-footer :footer="footer"></site-footer>
+
     
   </v-app>
   
 </template>
 
 <script>
-
-
+import SiteTitle from '@/views/site/title'
+import SiteFooter from '@/views/site/footer'
+import SiteMenu from '@/views/site/menu'
 export default {
+  components: {SiteTitle,SiteFooter, SiteMenu},
   name: 'App',
   data(){
     return {
-      drawer:false
+      drawer:false,
+      title:'초원세탁소',
+      footer:'푸터입니다'
     }
   }
 
